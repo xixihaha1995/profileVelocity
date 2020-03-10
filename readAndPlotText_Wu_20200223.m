008713cce5a7fd075d9beaa44f770972e34c0761
lichen <lichen@hydrolab.com>
2020-03-10 16:55:18 -0400

    to find best three jet imgs

--- readAndPlotText_Wu_20200223.m
+++ readAndPlotText_Wu_20200223.m
@@ -16,6 +16,7 @@ ext = '.txt';
 ext_img = '.bmp';
 ext_out = '.txt';
 
+go_on ='y';
 
 currentDate = 20200107;
 % 20200107 is blank
@@ -23,7 +24,6 @@ currentDate = 20200107;
 currentNdl = input('currentNdl: ');
 currentHight = input('currentHight:  ');
 currentRun = input('currentRun:  ');
-
 ii = input('ii:  ');
 
 
@@ -44,58 +44,76 @@ prefix_9 ='_r';
 prefix_4 =num2str(currentRun);
 prefix_5= '_';
 
-prefix = strcat(prefix_1,prefix_15,prefix_10,prefix_11,prefix_6,...
-    prefix_7,prefix_2,prefix_3,prefix_12,prefix_13,prefix_8,prefix_9,prefix_4,prefix_5,num2str(ii, '%05g'),ext);
 
-disp(prefix)
-disp('------------------')
+while go_on == 'Y' | go_on == 'y'
 
-prefix_img = strcat(prefix_1,prefix_15,prefix_10,prefix_11,prefix_6,...
-    prefix_7,prefix_2,prefix_3,prefix_12,prefix_13,prefix_8,prefix_9,prefix_4,prefix_5,num2str(ii, '%05g'),ext_img);
-% 
-% prefix_1 = 'C:\Users\lab-admin\Desktop\Lichen_Wu\movies_processed\';
-% prefix_14 = num2str(currentDate);
-% prefix_15 = '_ndl';
-% prefix_10 = num2str(currentNdl);
-% prefix_11 = '_hgt';
-% prefix_6 = num2str(currentHight);
-% prefix_7 ='_r';
-% prefix_2 = num2str(currentRun);
-% prefix_3 =strcat('\',num2str(currentDate),'_ndl');
-% prefix_12 = num2str(currentNdl);
-% prefix_13 = '_ht';
-% prefix_8 =num2str(currentHight);
-% prefix_9 ='_r';
-% prefix_4 =num2str(currentRun);
-% prefix_5= '_';
-% 
-% prefix = strcat(prefix_1,prefix_14,prefix_15,prefix_10,prefix_11,prefix_6,...
-%     prefix_7,prefix_2,prefix_3,prefix_12,prefix_13,prefix_8,prefix_9,prefix_4,prefix_5);
+    prefix = strcat(prefix_1,prefix_15,prefix_10,prefix_11,prefix_6,...
+        prefix_7,prefix_2,prefix_3,prefix_12,prefix_13,prefix_8,prefix_9,prefix_4,prefix_5,num2str(ii, '%05g'),ext);
 
-outDir = 'C:\Users\lab-admin\Desktop\Lichen_Wu\matlab\profileVelocity\';
-
-filename_out = strcat(outDir,'JetVel',ext_out);
+    disp(prefix)
 
 
 
-fileID = fopen(prefix);
-C = textscan(fileID, '%f %f');
 
-yourImage = imread(prefix_img);
-imshow(yourImage);
-hold on;
+    prefix_img = strcat(prefix_1,prefix_15,prefix_10,prefix_11,prefix_6,...
+        prefix_7,prefix_2,prefix_3,prefix_12,prefix_13,prefix_8,prefix_9,prefix_4,prefix_5,num2str(ii, '%05g'),ext_img);
+    % 
+    % prefix_1 = 'C:\Users\lab-admin\Desktop\Lichen_Wu\movies_processed\';
+    % prefix_14 = num2str(currentDate);
+    % prefix_15 = '_ndl';
+    % prefix_10 = num2str(currentNdl);
+    % prefix_11 = '_hgt';
+    % prefix_6 = num2str(currentHight);
+    % prefix_7 ='_r';
+    % prefix_2 = num2str(currentRun);
+    % prefix_3 =strcat('\',num2str(currentDate),'_ndl');
+    % prefix_12 = num2str(currentNdl);
+    % prefix_13 = '_ht';
+    % prefix_8 =num2str(currentHight);
+    % prefix_9 ='_r';
+    % prefix_4 =num2str(currentRun);
+    % prefix_5= '_';
+    % 
+    % prefix = strcat(prefix_1,prefix_14,prefix_15,prefix_10,prefix_11,prefix_6,...
+    %     prefix_7,prefix_2,prefix_3,prefix_12,prefix_13,prefix_8,prefix_9,prefix_4,prefix_5);
 
-x = C{1};
-y = 1000 - C{2};
+    outDir = 'C:\Users\lab-admin\Desktop\Lichen_Wu\matlab\profileVelocity\';
 
-[heightYY,minH_index] = min(y);
-heightXX = x(minH_index);
-
-plot(x,y);
+    filename_out = strcat(outDir,'JetVel',ext_out);
 
 
-fid = fopen(filename_out,'a');
-fprintf(fid, '%d \t %d \t %d \t %d \t %d \t  %d \t  %d \t  %d \t  %d \t  %d \t %d \t  %d \t %8.2f\n',...
-    [currentDate;currentNdl;currentHight;currentRun;c(1);c(2);c(3);c(4);c(5);c(6);ii;heightXX;heightYY]); %relative to flat surface
-fclose(fid);
+
+    fileID = fopen(prefix);
+    C = textscan(fileID, '%f %f');
+
+    yourImage = imread(prefix_img);
+    imshow(yourImage);
+    hold on;
+
+    x = C{1};
+    y = 1000 - C{2};
+
+    [heightYY,minH_index] = min(y);
+    heightXX = x(minH_index);
+    fprintf('%d  %d',heightXX,heightYY);
+
+    plot(x,y);
+
+
+    fid = fopen(filename_out,'a');
+    fprintf(fid, '%d \t %d \t %d \t %d \t %d \t  %d \t  %d \t  %d \t  %d \t  %d \t %d \t  %d \t %8.2f\n',...
+        [currentDate;currentNdl;currentHight;currentRun;c(1);c(2);c(3);c(4);c(5);c(6);ii;heightXX;heightYY]); %relative to flat surface
+    fclose(fid);
+
+
+    go_on = input('Continue to process NEXT image? Y/N [Y]: ','s');
+    go_on ='';
+        if isempty(go_on)
+            go_on = 'Y';
+            i = i+1;
+        elseif go_on =='n'
+            break   
+end
+
+disp('---------------')
 diary jetVelocities
